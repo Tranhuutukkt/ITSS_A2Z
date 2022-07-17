6e69135cee9b90a7ede3f50e8a5788626a0e4643
Trần Hữu An <tranhuutukkt@gmail.com>
2022-07-17 14:13:27 +0700

    fixx

--- src/components/myProfile.jsx
+++ src/components/myProfile.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import {getCurrentUser} from "../services/authService";
 import MyTab from "./myTab";
-import {Route, Switch} from "react-router-dom";
+import {Redirect, Route, Switch} from "react-router-dom";
 import ProfileInfo from "./profileInfo";
+import Posts from "./posts";
 
 function MyProfile({user}) {
     const tabList = [
@@ -15,18 +16,20 @@ function MyProfile({user}) {
 
     return (
         <React.Fragment>
-            {user && (
-                <div className='profile-tabs'>
-                    <MyTab
-                        tabList={user.role === 'teacher' ?
-                            tabList.filter(t => t.label !== 'comment') :
-                            tabList.filter(t => t.label !== 'answer')}
-                    />
-                </div>
-            )}
             <main>
+                {user && (
+                    <div className='profile-tabs'>
+                        <MyTab
+                            tabList={user.role === 'teacher' ?
+                                tabList.filter(t => t.label !== 'comment') :
+                                tabList.filter(t => t.label !== 'answer')}
+                        />
+                    </div>
+                )}
                 <Switch>
                     <Route path='/me/profile' exact component={ProfileInfo}/>
+                    <Route path='/me/posts' exact component={Posts}/>
+                    {/*<Redirect to='/me/profile'/>*/}
                 </Switch>
             </main>
         </React.Fragment>
--- src/components/myTab.jsx
+++ src/components/myTab.jsx
@@ -1,20 +1,19 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 
 function MyTab({tabList}) {
     //eg. tabList = [{label: 'home', title: 'Home'}]
-    const key = tabList[0].label;
 
     return (
         <React.Fragment>
             <ul className="nav nav-tabs nav-justified">
                 {tabList.map(t => (
                     <li className="nav-item" key={t.label}>
-                        <Link
-                            className={key === t.label ? "nav-link active show": "nav-link"}
+                        <NavLink
+                            className="nav-link"
                             aria-current="page"
                             to={'/me/' + t.label}
-                        >{t.title}</Link>
+                        >{t.title}</NavLink>
                     </li>
                 ))}
             </ul>
